72476c2501ec0bcf98d7ad9cba100b08c6944802
“ErwiNiwre” <niwre7690@gmail.com>
2018-11-03 03:36:02 -0400

    Plantilla de AdminLTE

---
+++ resources/views/layouts/incos_inicio.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  {{-- <title>Incos - El Alto</title> --}}
+  <title>@yield('titulo')</title>
+
+  {{-- <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"> --}}
+  {{-- Boostrap 3.3.7 --}}
+  <link rel="stylesheet" href="{!! asset('/adminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css') !!}">
+  {{-- Font Awesome --}}
+  <link rel="stylesheet" href="{!! asset('/adminLTE/bower_components/font-awesome/css/font-awesome.min.css') !!}">
+  {{-- Ionicons --}}
+  <link rel="stylesheet" href="{!! asset('/adminLTE/bower_components/Ionicons/css/ionicons.min.css') !!}">
+  {{-- Theme style --}}
+  <link rel="stylesheet" href="{!! asset('/adminLTE/css/AdminLTE.min.css') !!}">
+  <link rel="stylesheet" href="{!! asset('/adminLTE/css/skins/skin-green.min.css') !!}">
+
+  {{-- DataTables --}}
+  <link rel="stylesheet" href="{!! asset('/adminLTE/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') !!}">
+  
+  {{--  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>  --}}
+  {{--  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>  --}}
+
+  <!-- Google Font -->
+  <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+
+<!--
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to get the
+desired effect
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+-->
+
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  @include('layouts.header')
+  
+  <!-- sidebar -->
+  @include('layouts.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    {{-- <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section> --}}
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+      {{-- <!-------------------------- --}}
+        {{-- | Your Page Content Here | --}}
+          @yield('content')  
+      {{-- --------------------------> --}}
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  @include('layouts.footer') 
+
+ 
+  <div class="control-sidebar-bg">
+    
+  </div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 3 -->
+<script src="{!! asset('/adminLTE/bower_components/jquery/dist/jquery.min.js') !!}"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="{!! asset('/adminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js') !!}"></script>
+<!-- AdminLTE App -->
+<script src="{!! asset('/adminLTE/js/adminlte.min.js') !!}"></script>
+
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,104 @@
+<aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="/adminLTE/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <!-- Status -->
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- search form (Optional) -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+              </button>
+            </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+
+      <!-- Sidebar Menu -->
+      <hr color="red">
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MENU</li>
+        <!-- Optionally, you can add icons to the links -->
+        <li class="active"><a href="#"><i class="fa fa-home"></i> <span>Inicio</span></a></li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-mortar-board"></i> <span>Carreras</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Administración de Empresas</a></li>
+            <li><a href="#">Comercio Internacional</a></li>
+            <li><a href="#">Contaduria General</a></li>
+            <li><a href="#">Linguistica</a></li>
+            <li><a href="#">Secretariado Ejecutivo</a></li>
+            <li><a href="#">Sistemas Informáticos</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#"><i class="fa fa-bullhorn"></i> <span>Admisiones</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Información</a></li>
+            <li><a href="#">Convocatoria</a></li>
+            <li><a href="#">Formulario</a></li>
+            <li><a href="#">Resultados</a></li> 
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-newspaper-o"></i> <span>Noticia</span></a></li>
+        <li><a href="#"><i class="fa fa-info-circle"></i> <span>Nosotros</span></a></li>
+        <li><a href="#"><i class="fa fa-envelope"></i> <span>Contacto</span></a></li>
+      </ul>
+
+      <ul class="sidebar-menu" data-widget="tree">
+          <li class="header">MENU ADMINSITRADOR</li>
+          <!-- Optionally, you can add icons to the links -->
+          <li class="treeview">
+            <a href="#"><i class="fa fa-mortar-board"></i> <span>REGISTRAR</span>
+              <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+            </a>
+            <ul class="treeview-menu">
+              {{-- <li><a href="{{ route('administrativos.index') }}">ADMINISTRATIVOS</a></li>
+              <li><a href="{{ route('docentes.index') }}">DOCENTES</a></li> --}}
+              {{-- <li><a href="{{ route('estudiantes.index') }}">ESTUDIANTES</a></li> --}}
+            </ul>
+          </li>
+          <li class="treeview">
+            <a href="#"><i class="fa fa-bullhorn"></i> <span>Admisiones</span>
+              <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+            </a>
+            <ul class="treeview-menu">
+              <li><a href="#">Información</a></li>
+              <li><a href="#">Convocatoria</a></li>
+              <li><a href="#">Formulario</a></li>
+              <li><a href="#">Resultados</a></li> 
+            </ul>
+          </li>
+          <li><a href="#"><i class="fa fa-newspaper-o"></i> <span>Noticia</span></a></li>
+          <li><a href="#"><i class="fa fa-info-circle"></i> <span>Nosotros</span></a></li>
+          <li><a href="#"><i class="fa fa-envelope"></i> <span>Contacto</span></a></li>
+        </ul>
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
